4c5098aaaf7c709d9a0032be8651f102e3f05f3e
Junior <143552927+GJuniorr@users.noreply.github.com>
2024-10-07 06:41:02 -0300

    Update addDespesap.dart

--- lib/screens/rendaPessoal/adcionar/addDespesap.dart
+++ lib/screens/rendaPessoal/adcionar/addDespesap.dart
@@ -34,10 +34,10 @@ class _addDespesaState extends State<addDespesa> {
 
   Future getConnection() async {
     conn = await MySQLConnection.createConnection(
-      host: '10.0.2.2',
-      port: 3306,
-      userName: 'root',
-      password: '@Kinafox223',
+       host: '',
+       port: ,
+       userName: '',
+       password: '',
       databaseName: 'appmultiuso',
     );
     try {
