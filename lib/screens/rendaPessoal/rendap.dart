72b5dfc1d6e29ecdf2c97a5b5c8cfd3c06715b4e
Junior <143552927+GJuniorr@users.noreply.github.com>
2024-10-07 06:40:43 -0300

    Update rendap.dart

--- lib/screens/rendaPessoal/rendap.dart
+++ lib/screens/rendaPessoal/rendap.dart
@@ -63,10 +63,10 @@ class _rendaState extends State<renda> {
 
   Future getConnection() async {
     conn = await MySQLConnection.createConnection(
-      host: '10.0.2.2',
-      port: 3306,
-      userName: 'root',
-      password: '@Kinafox223',
+      host: '',
+      port: ,
+      userName: '',
+      password: '',
       databaseName: 'appmultiuso',
     );
     try {
