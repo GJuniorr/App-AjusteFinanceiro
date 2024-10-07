4e222f0048bbc0908d7f733ce702b518c708392d
Junior <143552927+GJuniorr@users.noreply.github.com>
2024-10-07 06:36:53 -0300

    Update login.dart

--- lib/login.dart
+++ lib/login.dart
@@ -25,10 +25,10 @@ class _loginState extends State<login> {
 
   Future getConnection() async {
     conn = await MySQLConnection.createConnection(
-      host: '10.0.2.2',
-      port: 3306,
-      userName: 'root',
-      password: '@Kinafox223',
+      host: '',
+      port: ,
+      userName: '',
+      password: '',
       databaseName: 'appmultiuso',
     );
     try {
